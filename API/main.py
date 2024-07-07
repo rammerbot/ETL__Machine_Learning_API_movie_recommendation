3f50eaeaeb050d8a45feef234958449812d3caa5
unknown <rammer94@gmail.com>
2024-07-07 19:20:37 -0400

    Mayusculas modelo2

--- API/main.py
+++ API/main.py
@@ -64,5 +64,5 @@ def get_director(director:str):
 # Recomendar peliculas
 @app.get("/recommender/", tags=['Search'])
 def recomendacion(titulo:str):
-    recomendacion = recommender(titulo.lower())
+    recomendacion = recommender(titulo)
     return recomendacion
